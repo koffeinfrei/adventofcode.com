ec00d123fe0caeee0a79349560addd744ef45e9f
Alexis Reigel <mail@koffeinfrei.org>
2023-12-09 22:30:37 +0100

    2023 08 part 2, lcm

--- 2023/08_2/08_2.rb
+++ 2023/08_2/08_2.rb
@@ -5,16 +5,16 @@ instructions = lines.shift.chars.map { ['L', 'R'].index(_1) }
 map = lines.map { _1.scan(/(\w{3}) = \((\w{3}), (\w{3})\)/).flatten }.map { |(start, left, right)| [start, [left, right]] }.to_h
 
 current_keys = map.keys.select { _1.end_with?('A') }
-steps = 0
+steps = Array.new(current_keys.length, 0)
 
-until current_keys.all? { _1.end_with?('Z') }
-  instructions.each do |instruction|
-    steps += 1
-    current_keys.each.with_index do |current, index|
-      current_keys[index] = map[current][instruction]
+current_keys.each.with_index do |_, index|
+  until current_keys[index].end_with?('Z')
+    instructions.each do |instruction|
+      steps[index] += 1
+      current_keys[index] = map[current_keys[index]][instruction]
+      break if current_keys[index].end_with?('Z')
     end
-    break if current_keys.all? { _1.end_with?('Z') }
   end
 end
 
-puts steps
+puts steps.reduce(:lcm)
