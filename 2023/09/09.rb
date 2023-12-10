9eae3bacdb7d9122f6b08ba4cb3ef0c046ece405
Alexis Reigel <mail@koffeinfrei.org>
2023-12-10 10:14:06 +0100

    2023 09

---
+++ 2023/09/09.rb
@@ -0,0 +1,23 @@
+lines = File.readlines('./input').map(&:strip)
+
+histories = lines.map { [_1.scan(/-*\d+/).map(&:to_i)] }
+
+histories.each do |history|
+  # create a diff sequence until we have one with all zeros
+  until history.last.all?(&:zero?)
+    last = history.last
+    history << []
+    1.upto(last.length - 1).each do |index|
+      history.last << last[index] - last[index - 1]
+    end
+  end
+
+  # start extrapolating by adding another 0 to the last sequence...
+  history.last << 0
+  # ... and the adding the next number for each line above
+  (history.length - 2).downto(0).each do |index|
+    history[index] << history[index][-1] + history[index + 1][-1]
+  end
+end
+
+puts histories.map(&:first).map(&:last).sum
